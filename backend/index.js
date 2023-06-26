a1999b8c0e23dd28040fa977ac8cd6496739be31
jose <albertojoseponce@gmail.com>
2023-06-26 20:58:01 -0300

    subir las rutas

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,19 @@ const morgan = require ('morgan')
 app.set('puerto' , 2023);
 app.use(morgan('dev'))
 
+app.get('/', (req , res)=>{
+    res.send('hola chicos')
+})
+
+app.get('/hola', (req , res)=>{
+    res.send('hola chicos esto ingreso aca ahora')
+})
+
+app.post('/saludo', (req , res)=>{
+    res.send('<h1>gg saludos</h1>hola chicos esto esta entrando en el metodo post')
+})
+
 app.listen(app.get('puerto'), ()=>{
-    console.log('El serv del profe esta corriendo en el puerto', app.get('puerto'))
+    console.log('El servidor del profe esta corriendo en el puerto', app.get('puerto'))
 })
 
