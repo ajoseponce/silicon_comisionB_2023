e28af7cd61a695851a3c06dfdd64c0d7944619dd
jose <albertojoseponce@gmail.com>
2023-07-03 20:56:15 -0300

    instalaciond e body parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,12 @@ const express=require('express');
 const app = express();
 
 const morgan = require ('morgan')
+
 app.set('puerto' , 2023);
+
 app.use(morgan('dev'))
+
+
 
 app.use(require('./routes/routes'))
 
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,19 +1,53 @@
 const express = require('express');
 
 const coneccion = require('../database/bd')
+const bodyParser = require('body-parser');
 
 const router = express()
-
-router.get('/', (req , res)=>{
-    res.send('esta es la ruta de inicio')
-})
-
-router.get('/equipos', (req , res)=>{
-    res.send('hola chicos esta es la ruta del listado de equipos.')
-})
-
-// app.post('/saludo', (req , res)=>{
-//     res.send('<h1>gg saludos</h1>hola chicos ')
+// router.use(bodyParser.json)
+// router.use(
+//     bodyParser.urlencoded({
+//       extended: true,
+//     }),
+//   );
+  
+// router.get('/', (req , res)=>{
+//     res.send('esta es la ruta de inicio')
 // })
+// /////////////rutas de modelos////////////////
+
+// listar modelos
+router.get('/modelos', (req , res)=>{
+    coneccion.query('SELECT m.id_modelo, m.nombre, f.nombre AS fabricante FROM modelos AS m INNER JOIN fabricantes AS f ON f.id_fabricante=m.id_fabricante', (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.json(registros)
+        }
+    })
+})
+
+// traer los  datos del modelo por ID
+router.get('/modelos/:id_modelo', (req , res)=>{
+    const { id_modelo } = req.params
+    coneccion.query('SELECT * FROM modelos WHERE id_modelo=?', [id_modelo], (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.json(registros)
+        }
+    })
+})
+////////////////////inserte los modulos
+router.post('/modelos', bodyParser.json(), (req , res)=>{
+     const { nombre, id_fabricante }  = req.body
+    // console.log('datos', req.body)
+    // console.log(id_fabricante)
+    // res.send('hola')
+})
+
+// /////////////////////////////
+
+
 
 module.exports= router;
