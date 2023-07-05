0a7165d48c44a90018ebb94238843757ea296b3b
jose <albertojoseponce@gmail.com>
2023-07-05 20:49:22 -0300

    endpoin de entidades -> tipo de equipo, ubicaicones, fabricantes

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -133,22 +133,194 @@ router.put('/fabricantes/:id_fabricante', bodyParser.json(), (req , res)=>{
    })
 })
 
-///////////////////edicion de fabricante
-// metodo PUT
+///////////////////eliminacion de fabricante
+// metodo DELETE
 //URL /fabricantes/:id_fabricante
 //parametros : 
-    // y el parametro que vamos a borrar logicamente ->id_fabricante
-    router.delete('/fabricantes/:id_fabricante', bodyParser.json(), (req , res)=>{
-    
-        const { id_fabricante } = req.params
-        mysqlConnect.query('UPDATE fabricantes SET estado = "B"  WHERE id_fabricante = ?', [id_fabricante], (error, registros)=>{
-           if(error){
-               console.log('Error en la base de datos', error)
-           }else{
-               res.send('El registro ' +id_fabricante+ ' se dio de baja correctamente')
-           }
-       })
+// y el parametro que vamos a borrar logicamente ->id_fabricante
+router.delete('/fabricantes/:id_fabricante', bodyParser.json(), (req , res)=>{
+
+    const { id_fabricante } = req.params
+    mysqlConnect.query('UPDATE fabricantes SET estado = "B"  WHERE id_fabricante = ?', [id_fabricante], (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.send('El registro ' +id_fabricante+ ' se dio de baja correctamente')
+        }
     })
-    
+})
+
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+// listar tipos de equipo
+// metodo GET
+//URL /tipos_equipo
+//parametros : ninguno
+router.get('/tipos_equipo', (req , res)=>{
+    mysqlConnect.query('SELECT * FROM tipos_equipo ', (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.json(registros)
+        }
+    })
+})
+// traer los  datos del tipos_equipo por el ID
+
+// metodo GET
+//URL /tipos_equipo/:id_tipo_equipo
+//parametros : ninguno
+router.get('/tipos_equipo/:id_tipo_equipo', (req , res)=>{
+    const { id_tipo_equipo } = req.params
+    mysqlConnect.query('SELECT * FROM tipos_equipo WHERE id_tipo_equipo=?', [id_tipo_equipo], (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.json(registros)
+        }
+    })
+})
+////////////////////insert de tipos equipo
+
+// metodo POST
+//URL /tipos_equipo/
+//parametros : en el cuerpo(body) 
+    // nombre
+
+router.post('/tipos_equipo', bodyParser.json(), (req , res)=>{
+    const { nombre }  = req.body
+  
+    mysqlConnect.query('INSERT INTO tipos_equipo (nombre) VALUES (?)', [nombre], (error, registros)=>{
+       if(error){
+           console.log('Error en la base de datos', error)
+       }else{
+           res.send('El insert se realizo correctamente')
+       }
+   })
+})
+
+
+////////////////////edicion de tipos de equipo
+// metodo PUT
+//URL /tipos_equipo/:id_tipo_equipo
+//parametros : 
+    // en el cuerpo(body) 
+    // nombre
+    // y el parametro que vamos a editar ->id_tipo_equipo
+router.put('/tipos_equipo/:id_tipo_equipo', bodyParser.json(), (req , res)=>{
+    const { nombre }  = req.body
+    const { id_tipo_equipo } = req.params
+    mysqlConnect.query('UPDATE tipos_equipo SET nombre = ?  WHERE id_tipo_equipo = ?', [nombre, id_tipo_equipo], (error, registros)=>{
+       if(error){
+           console.log('Error en la base de datos', error)
+       }else{
+           res.send('La edicion de registro ' +id_tipo_equipo+ ' se realizo correctamente')
+       }
+   })
+})
+
+///////////////////eliminacion de tipos de equipo
+// metodo DELETE
+//URL /tipos_equipo/:id_tipo_equipo
+//parametros : 
+    // y el parametro que vamos a borrar logicamente ->id_tipo_equipo
+router.delete('/tipos_equipo/:id_tipo_equipo', bodyParser.json(), (req , res)=>{
+    const { id_tipo_equipo } = req.params
+    mysqlConnect.query('UPDATE tipos_equipo SET estado = "B"  WHERE id_tipo_equipo = ?', [id_tipo_equipo], (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.send('El registro ' +id_tipo_equipo+ ' se dio de baja correctamente')
+        }
+    })
+})
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+//////////////////////////////
+// listar tipos de equipo
+// metodo GET
+//URL /ubicaciones
+//parametros : ninguno
+router.get('/ubicaciones', (req , res)=>{
+    mysqlConnect.query('SELECT * FROM ubicaciones ', (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.json(registros)
+        }
+    })
+})
+// traer los  datos del ubicaciones por el ID
+
+// metodo GET
+//URL /ubicaciones/:id_ubicacion
+//parametros : ninguno
+router.get('/ubicaciones/:id_ubicacion', (req , res)=>{
+    const { id_ubicacion } = req.params
+    mysqlConnect.query('SELECT * FROM ubicaciones WHERE id_ubicacion=?', [id_ubicacion], (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.json(registros)
+        }
+    })
+})
+////////////////////insert de tipos equipo
+
+// metodo POST
+//URL /ubicaciones/
+//parametros : en el cuerpo(body) 
+    // nombre
+
+router.post('/ubicaciones', bodyParser.json(), (req , res)=>{
+    const { nombre }  = req.body
+  
+    mysqlConnect.query('INSERT INTO ubicaciones (nombre) VALUES (?)', [nombre], (error, registros)=>{
+       if(error){
+           console.log('Error en la base de datos', error)
+       }else{
+           res.send('El insert se realizo correctamente')
+       }
+   })
+})
+
+
+////////////////////edicion de tipos de equipo
+// metodo PUT
+//URL /ubicaciones/:id_ubicacion
+//parametros : 
+    // en el cuerpo(body) 
+    // nombre
+    // y el parametro que vamos a editar ->id_ubicacion
+router.put('/ubicaciones/:id_ubicacion', bodyParser.json(), (req , res)=>{
+    const { nombre }  = req.body
+    const { id_ubicacion } = req.params
+    mysqlConnect.query('UPDATE ubicaciones SET nombre = ?  WHERE id_ubicacion = ?', [nombre, id_ubicacion], (error, registros)=>{
+       if(error){
+           console.log('Error en la base de datos', error)
+       }else{
+           res.send('La edicion de registro ' +id_ubicacion+ ' se realizo correctamente')
+       }
+   })
+})
+
+///////////////////eliminacion de tipos de equipo
+// metodo DELETE
+//URL /ubicaciones/:id_ubicacion
+//parametros : 
+    // y el parametro que vamos a borrar logicamente ->id_ubicacion
+router.delete('/ubicaciones/:id_ubicacion', bodyParser.json(), (req , res)=>{
+    const { id_ubicacion } = req.params
+    mysqlConnect.query('UPDATE ubicaciones SET estado = "B"  WHERE id_ubicacion = ?', [id_ubicacion], (error, registros)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            res.send('El registro ' +id_ubicacion+ ' se dio de baja correctamente')
+        }
+    })
+})
 
 module.exports= router;
