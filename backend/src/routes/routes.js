f170f9cb3de7f53420bf002b92421ca3bd4fca64
jose <albertojoseponce@gmail.com>
2023-07-17 20:44:04 -0300

    endpoint del login

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -14,7 +14,8 @@ router.get('/', (req , res)=>{
 // registro de usuarios
 router.post('/registro', bodyParser.json() , (req , res)=>{
     const {apellido, nombre , dni, user, pass, correo, id_rol} =req.body;
-    let hash= bcrypt.hashSync(pass, 5);
+    //
+    let hash= bcrypt.hashSync(pass, 10);
     //
     if(!dni){
         res.json({
@@ -46,6 +47,50 @@ router.post('/registro', bodyParser.json() , (req , res)=>{
             }
         }
     })
+})
+
+router.post('/login', bodyParser.json() , (req , res)=>{
+    const {user, pass} =req.body
+    console.log(user)
+    if(!user){
+        res.json({
+            status:false,
+            mensaje:"El user es un dato obligatorio para el login" 
+        }) 
+    }
+    if(!pass){
+        res.json({
+            status:false,
+            mensaje:"El password es un dato obligatorio para el login" 
+        }) 
+    }
+    mysqlConnect.query('SELECT * FROM usuarios WHERE user=?', [user], (error, usuario)=>{
+        if(error){
+            console.log('Error en la base de datos', error)
+        }else{
+            if(usuario.length>0){
+                 
+                 const comparacion= bcrypt.compareSync(pass, usuario[0].pass)   
+                 if(comparacion)  {
+                    res.json({
+                        status: true
+                    }) 
+                 }else{
+                    res.json({
+                        status:false,
+                        mensaje:"La contraseña es incorrecta" 
+                    }) 
+                 }
+            }else{
+                res.json({
+                    status:false,
+                    mensaje:"El usuario NO EXISTE" 
+                }) 
+            }
+        }
+    })
+    
+
 })
 
 
